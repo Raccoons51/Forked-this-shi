1a4d4970b10a0958fe30f4ff3a50db2ae4b6a503
Snoopy <118954786+Snoopy975@users.noreply.github.com>
2026-09-17 17:42:43 -0700

    started work on turret and limelight

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/TurretAutoAlign.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.Subsystem;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TurretAutoAlign extends OpMode {
+    private AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
